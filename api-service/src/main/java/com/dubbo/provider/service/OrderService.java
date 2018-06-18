3701de698fd5d2d7d564d03d3470d86495b64b1b
Yuanzr <81491531@163.com>
2018-06-19 00:26:23 +0800

    init project

---
+++ api-service/src/main/java/com/dubbo/provider/service/OrderService.java
@@ -0,0 +1,5 @@
+package com.dubbo.provider.service;
+
+public interface OrderService {
+    void addBill();
+}
---
+++ api-service/src/main/java/com/dubbo/provider/service/ProviderService.java
@@ -0,0 +1,5 @@
+package com.dubbo.provider.service;
+
+public interface ProviderService {
+    String pSayHello(String name);
+}
